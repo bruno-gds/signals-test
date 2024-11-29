211a71e807e64dcb6f17f2b349d92b8ea2048fdf
Bruno Gomes <brunog.damascena@gmail.com>
2024-11-29 18:10:59 -0300

    refatorando

--- src/app/body/children/children.component.ts
+++ src/app/body/children/children.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, signal, Signal} from '@angular/core';
+import {Component, computed} from '@angular/core';
 
 import {ContadorService} from "../../service/contador.service";
 
@@ -12,14 +12,12 @@ import {ContadorService} from "../../service/contador.service";
 })
 export class ChildrenComponent {
 
-  contador: Signal<number> = signal(0);
+  contador = computed(() => {
+    console.log("Signal ChildrenComponent - atualizando......");
+    return this.contadorService.valorAtualContadorSignal()
+  });
 
   constructor(protected readonly contadorService: ContadorService) {
     console.log("Iniciando ChildrenComponent......");
-
-    this.contador = computed(() => {
-      console.log("Signal - ChildrenComponent");
-      return contadorService.valorAtualContadorSignal()
-    })
   }
 }
